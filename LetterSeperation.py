fd58458012f08d655fd0acec3516778d1cf31e16
JoelMathewC <joelmathewcherian@gmail.com>
2020-07-29 15:27:28 +0400

    Model Loaded

--- LetterSeperation.py
+++ LetterSeperation.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import tensorflow as tf 
+import keras
 
 img = cv2.imread('omega.jpg',0)
 
@@ -9,6 +11,10 @@ cv2.waitKey(0)
 _,thresh = cv2.threshold(img,116,255,cv2.THRESH_BINARY_INV)
 contours,_ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
 i = 0
+recognizer = tf.keras.models.load_model('emnist_classifier_mark2.h5')
+
+
+
 for cnt in contours:
     i = i+1
     if cv2.contourArea(cnt) > 500:
@@ -17,6 +23,16 @@ for cnt in contours:
         cropped = thresh[y:y+h,x:x+w]
         cropped = cv2.resize(cropped,(20,20))
         cropped = cv2.copyMakeBorder(cropped,4,4,4,4,cv2.BORDER_CONSTANT,value = (0,0,0))
+        
+        cropped = np.asarray(cropped)
+        cropped = cropped.reshape(-1,28,28,1)
+        cropped = cropped/255
+        pred = recognizer.predict_classes(cropped)
+        print(pred)
+
+       
+
+
         cv2.imwrite(str(i)+'s.jpg',cropped)
         cv2.imshow('Image',thresh)
         cv2.waitKey(0)
